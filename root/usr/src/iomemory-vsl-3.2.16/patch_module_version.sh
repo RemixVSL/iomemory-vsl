aad6d57eaf583647572a7126230cc2ecb042c22e
Funs <fozzielumpkins@gmail.com>
2020-02-07 09:04:18 -0800

    Modified the Makefile to: 1) Add the module version to the license.c before compiling, resulting in a version string on modinfo: ``` modinfo ./iomemory-vsl.ko  | grep version version:        d663292 ```
    2) patch the module binary to reflect the version that is in use.
    ```
    dmesg | grep "ioDrive driver"
    old:
    [174199.774438] <6>fioinf ioDrive driver 3.2.16.1731 pinnacles@4870ad45b7ea loading...
    [174319.199644] <6>fioinf ioDrive driver unloading...
    [174319.285798] <6>fioinf ioDrive driver unloaded...
    
    new:
    [175631.667841] <6>fioinf ioDrive driver 3.2.16.1731 4870ad45b7ea d663292 a loading...
    ```

--- root/usr/src/iomemory-vsl-3.2.16/patch_module_version.sh
+++ root/usr/src/iomemory-vsl-3.2.16/patch_module_version.sh
@@ -3,10 +3,9 @@
 # Script to patch the iomemory-vsl.ko binary. It removes the username
 # and appends the first seven characters of the commit hash.
 #
-set -e
 
 module=$1
-sumtag=$2
+tag=$2
 
 if [ "$tag" == "" ]; then
     echo "patch_version not found, attempting to use git"
@@ -16,13 +15,30 @@ if [ "$tag" == "" ]; then
     echo "Error: git version tag missing, unable to tag $module"
     exit 1
 fi
-maxlength=$(perl -ne 'print length($1)."\n" if /(\d.\d.\d+.\d+ \w+@[\w\d]+)/' $module | head -1)
-newlength=$(perl -sne 'print length("$2 $4 $tag a")."\n" if /((\d.\d.\d+.\d+) (\w+@([\w\d]+)))/' -- -tag=$tag $module | head -1)
-if [ "$maxlength" == "$newlength" -a "$maxlength" == "34" ]; then
-    replace=$(perl -sne 'print "s/$1/$2 $4 $tag a/\n" if /((\d.\d.\d+.\d+) (\w+@([\w\d]+)))/' -- -tag=$tag $module | head -1)
-    sed -i "$replace" $module
-    echo "Ok: altered version for $module appended $tag"
+
+if [ -e $module ]; then
+    maxlength=$(perl -ne 'print length($1)."\n" if /(\d.\d.\d+.\d+ \w+@[\w\d]+)/' $module | head -1)
+    newlength=$(perl -sne 'print length("$2 $4 $tag a")."\n" if /((\d.\d.\d+.\d+) (\w+@([\w\d]+)))/' -- -tag=$tag $module | head -1)
+    if [ "$maxlength" == "$newlength" -a "$maxlength" == "34" ]; then
+        replace=$(perl -sne 'print "s/$1/$2 $4 $tag a/\n" if /((\d.\d.\d+.\d+) (\w+@([\w\d]+)))/' -- -tag=$tag $module | head -1)
+        sed -i "$replace" $module
+        echo "Ok: altered version for $module appended $tag"
+    elif [ "$maxlength" == "$newlength" -a "$maxlength" == "" ]; then
+        echo "Error: altering $module, module already altered it seems, string lengths don't make sense."
+        exit 1
+    else
+        echo "Error: altering $module, $maxlength is not $newlength"
+        exit 1
+    fi
 else
-    echo "Error: altering $module, $maxlength is not $newlength"
+    src=license.c
+    echo "Adding module version $tag to source $src"
+    grep -q MODULE_VERSION $src
+    if [ "$?" == "0" ]; then
+        replace=$(perl -sne 'print "s/$2/$tag/\n" if /(MODULE_VERSION)\(\"([\d\w]+)\"\)/' -- -tag=$tag $src | head -1)
+        sed -i "$replace" $src
+    else
+        echo "MODULE_VERSION(\""$tag"\");" >> $src
+    fi
     exit 1
-fi
+fi 
