80524c2a5183c8926aa64c6cb69cc88acd03f199
plappermaul <stockhausen@collogia.de>
2021-04-28 14:08:46 +0200

    add some more symbols
    object file is making some more sense now

--- root/usr/src/iomemory-vsl-3.2.16/include/fio/port/kinfo.h
+++ root/usr/src/iomemory-vsl-3.2.16/include/fio/port/kinfo.h
@@ -52,7 +52,7 @@ typedef int    kfio_info_text_handler_t(void *param, int cmd, kfio_info_data_t *
 
 extern kfio_info_node_t *g_fusion_info_root;
 extern kfio_info_node_t *g_fusion_info_fio;
-// extern kfio_info_node_t *g_fusion_info_iodrive;
+extern kfio_info_node_t *g_fusion_info_iodrive;
 
 extern int  kfio_info_create_dir(kfio_info_node_t *parent, const char *name, kfio_info_node_t **newdir);
 extern int  kfio_info_create_type(kfio_info_node_t *parent, const char *name, int type, fio_mode_t mode,
