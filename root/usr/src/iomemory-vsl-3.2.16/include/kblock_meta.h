778b75c53f3dcf28ab5b0eabe0941d2ac020ec48
Funs <fozzielumpkins@gmail.com>
2021-08-25 10:20:16 -0500

    splitting out _queue, queue_node and _disk_alloc. Also change the check in kblock.c to make sure KFIOC_X_HAS_MAKE_REQUEST_FN is set

--- root/usr/src/iomemory-vsl-3.2.16/include/kblock_meta.h
+++ root/usr/src/iomemory-vsl-3.2.16/include/kblock_meta.h
@@ -21,20 +21,21 @@
   static unsigned int kfio_make_request(struct request_queue *queue, struct bio *bio);
   #define BLK_QUEUE_SPLIT blk_queue_split(queue, &bio);
 
-  #if KFIOC_X_BLK_ALLOC_QUEUE_EXISTS
-    #define BLK_ALLOC_QUEUE blk_alloc_queue(kfio_make_request, node);
-  #else /* KFIOC_X_BLK_ALLOC_QUEUE_EXISTS not */
+  #if KFIOC_X_BLK_ALLOC_QUEUE_NODE_EXISTS
     #define BLK_ALLOC_QUEUE blk_alloc_queue_node(GFP_NOIO, node);
-  #endif /* KFIOC_X_BLK_ALLOC_QUEUE_EXISTS */
+  #else /* KFIOC_X_BLK_ALLOC_QUEUE_NODE_EXISTS */
+    #define BLK_ALLOC_QUEUE blk_alloc_queue(kfio_make_request, node);
+  #endif /* KFIOC_X_BLK_ALLOC_QUEUE_NODE_EXISTS */
 
 #else /* KFIOC_X_HAS_MAKE_REQUEST_FN */
   blk_qc_t kfio_submit_bio(struct bio *bio);
 
-  #if KFIOC_X_BLK_ALLOC_QUEUE_EXISTS
-    #define BLK_ALLOC_QUEUE blk_alloc_queue(node);
-  #else /* KFIOC_X_BLK_ALLOC_QUEUE_EXISTS not */
+  #if KFIOC_X_BLK_ALLOC_DISK_EXISTS
     #define BLK_ALLOC_QUEUE __blk_alloc_disk(node)->queue;
-  #endif /* KFIOC_X_BLK_ALLOC_QUEUE_EXISTS */
+  #else
+    #define BLK_ALLOC_QUEUE blk_alloc_queue(node);
+  #endif
+
   #define BLK_QUEUE_SPLIT blk_queue_split(&bio);
 #endif /* KFIOC_X_HAS_MAKE_REQUEST_FN */
 #if KFIOC_X_GENHD_PART0_IS_A_POINTER
@@ -42,6 +43,6 @@
   #define GET_BDEV bdgrab(disk->gd->part0)
 #else /* KFIOC_X_GENHD_PART0_IS_A_POINTER */
   #define GD_PART0 &gd->part0
-  #define GET_BDEV bdget_disk(disk->gd, 0);
+  #define GET_BDEV bdget_disk(disk->gd, 0)
 #endif /* KFIOC_X_GENHD_PART0_IS_A_POINTER */
 #endif /* __FIO_KBLOCK_META_H__ */
